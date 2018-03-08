1af0f8219a6290f3317474b0f851b4d7644a649d
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2018-03-09 00:22:07 +0100

    Add plotting utils:  broken in notebooks

---
+++ notebooks/utils.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+
+def plot_imgs(imgs, titles=None, cmap='brg', ylabel='', normalize=False, ax=None):
+    n = len(imgs)
+    if not isinstance(cmap, list):
+        cmap = [cmap]*n
+    if ax is None:
+        _, ax = plt.subplots(1, n, figsize=(6*n, 6), dpi=100)
+        if n == 1:
+            ax = [ax]
+    else:
+        if not isinstance(ax, list):
+            ax = [ax]
+        assert len(ax) == len(imgs)
+    for i in range(n):
+        if imgs[i].shape[-1] == 3:
+            imgs[i] = imgs[i][..., ::-1]  # BGR to RGB
+        ax[i].imshow(imgs[i], cmap=plt.get_cmap(cmap[i]),
+                     vmin=None if normalize else 0,
+                     vmax=None if normalize else 1)
+        if titles:
+            ax[i].set_title(titles[i])
+        ax[i].get_yaxis().set_ticks([])
+        ax[i].get_xaxis().set_ticks([])
+        for spine in ax[i].spines.values():  # remove frame
+            spine.set_visible(False)
+    ax[0].set_ylabel(ylabel)
+    plt.tight_layout()
