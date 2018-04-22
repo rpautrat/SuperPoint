2844edfe0b98d3b9f74b92cdee6be3ceced8db23
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2018-04-22 17:23:32 +0200

    Minor fixes for notebooks

--- superpoint/models/__init__.py
+++ superpoint/models/__init__.py
@@ -1,5 +1,5 @@
 def get_model(name):
-    mod = __import__('models.{}'.format(name), fromlist=[''])
+    mod = __import__('superpoint.models.{}'.format(name), fromlist=[''])
     return getattr(mod, _module_to_class(name))
 
 
