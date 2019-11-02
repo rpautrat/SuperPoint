Fixed normalization of images between 0 and 1

superpoint/datasets/patches_dataset.py
@@ -57,7 +57,7 @@ class PatchesDataset(BaseDataset):
             if config['preprocessing']['resize']:
                 image = pipeline.ratio_preserving_resize(image,
                                                          **config['preprocessing'])
-            return tf.to_float(image) / 255.
+            return tf.to_float(image)
 
         def _warp_image(image):
             H = sample_homography(tf.shape(image)[:2])
@@ -88,6 +88,9 @@ class PatchesDataset(BaseDataset):
                                                                   [path],
                                                                   tf.uint8))
         warped_images = warped_images.map(_preprocess)
+
+        images = images.map(lambda img: tf.to_float(img) / 255.)
+        warped_images = warped_images.map(lambda img: tf.to_float(img) / 255.)
 
         data = tf.data.Dataset.zip({'image': images, 'warped_image': warped_images,
                                     'homography': homographies})

superpoint/datasets/synthetic_shapes.py
@@ -1,4 +1,4 @@
-import numpy as np
+mport numpy as np
 import tensorflow as tf
 import cv2
 import os
@@ -160,13 +160,13 @@ class SyntheticShapes(BaseDataset):
                         **config['generation']['params']['generate_background'])
                 points = np.array(getattr(synthetic_dataset, primitive)(
                         image, **config['generation']['params'].get(primitive, {})))
-                yield (np.expand_dims(image, axis=-1).astype(np.float32) / 255.,
+                yield (np.expand_dims(image, axis=-1).astype(np.float32),
                        np.flip(points.astype(np.float32), 1))
 
         def _read_image(filename):
             image = tf.read_file(filename)
             image = tf.image.decode_png(image, channels=1)
-            return tf.cast(image, tf.float32) / 255.
+            return tf.cast(image, tf.float32)
 
         # Python function
         def _read_points(filename):
@@ -212,5 +212,6 @@ class SyntheticShapes(BaseDataset):
 
         # Convert the point coordinates to a dense keypoint map
         data = data.map_parallel(pipeline.add_keypoint_map)
+        data = data.map_parallel(lambda d: {**d, 'image': tf.to_float(d['image']) / 255.})
 
         return data