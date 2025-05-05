02a2cd91d73e8d1b243819f8f3cefbe0bc82cbd4
Andreas Klintberg <andreas.klintberg@gmail.com>
2025-05-04 18:56:24 -0700

    enable import of sub-packages for dataset creation

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='superpoint',
       version="0.0",
-      packages=['superpoint'])
+      packages=find_packages(include=['superpoint', 'superpoint.*'])
+)
